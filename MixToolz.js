ba4f0254b56703fabd2077d9f0f86c125e7c4542
Teng Ming Hein <tengminghein@gmail.com>
2024-04-30 13:30:15 +0800

    29/4 crop function

--- MixToolz.js
+++ MixToolz.js
@@ -83,3 +83,96 @@
       $(draw(e)).on("change", handleFileSelect);
       selDiv = $(reader.readAsDataURL(f));
     });
+
+import ReactCrop from "react-image-crop";
+
+const ASPECT_RATIO = 1;
+const MIN_DIMENSION = 150;
+
+const ImageCropper = () => {
+
+  const [imgSrc, setImgSrc] = useState('');
+  const [crop, setCrop] = useState();
+  const [error, selfError] = useState('')
+
+  const file = e.target.files?.[0];
+  if (!file) return;
+
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+      const imageElement = new Image();
+      const imageUrl = reader.result?.toString() || "";
+      imageElement.src = imageUrl;
+
+      imageElement.addEventListener("load", () => {
+          const { naturalwidth , naturalHeight } = e.currentTarget;
+          if (naturalwidth < MIN_DIMENSION || naturalHeight < MIN_DIMENSION) {
+              SetError("Image must be at least 150 x 150 pixels.");
+              setImgSrc("");
+              return setImgSrc("");
+          }
+      });
+      setImgSrc(imageUrl);
+  });
+  reader.readAsDataURL(file);
+};
+
+const onImageLoad =(e) => {
+  const { width , height } = e.currentTarget;
+  const cropweightInPercent = (MIN_DIMENSION / width) * 100;
+  
+  const crop = makeAspectCrop(
+      {
+          unit: "%",
+          width: cropweightInPercent,
+      }, 
+      ASPECT_RATIO,
+      width,
+      height
+  );
+  const centeredCrop = centerCrop(crop, width, height)
+  setCrop(centeredCrop);
+}
+
+class wall extends ReactCrop {
+  render(){
+    return(
+      <>
+        <label className="block mb-3 w-fit">
+            <span className="sr-only">Choose profile photo</span>
+            <input
+                type="file"
+                accept="image/*"
+                onChange={onSelectFile}
+                className="block w-full text-sm text-slate-500 file:mr-4 file:py-1 file:px-2 file:rounded-full file:border-0 file:text-xs file:bg-gray-700 file:text-sky-300 hover:file:bg-gray-600"
+            />
+        </label>
+        {error && <p className="text-red-400 text-xs">{error}</p>}
+        {imgSrc && 
+          <div className="flex flex-col items-center">{
+          <ReactCrop
+                  crop={crop}
+                  onChange={(pixelCrop, percentCrop)=>setCrop(percentlCrop)}                        
+                  squareCrop
+                  keepSelection
+                  aspect={ASPECT_RATIO}
+                  minWidth={MIN_DIMENSION}
+              >
+                  <img
+                      src={imgSrc}
+                      alt="Upload"
+                      style={{ maxHeight:"70vh" }}
+                      onload={onImageLoad}
+                  />
+              </ReactCrop>}
+              <button className="text-white font-nono text-xs py-2 px-4 rounded-2xl mt-4 bg-sky-500 hover:bg-sky-600">
+                  Crop Image
+              </button>
+          </div>
+        }
+      </>
+    )
+  }
+}
+
+ReactDom.render(<wall />, document.getElementById('input-file'))
