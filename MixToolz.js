f73f30a10ba44b3bc8776ec689e8b7ecc246f90e
Teng Ming Hein <tengminghein@gmail.com>
2024-05-04 21:12:06 +0800

    4/5 crop function

--- MixToolz.js
+++ MixToolz.js
@@ -16,8 +16,8 @@
   var selDiv = "";
       var storedFiles = [];
       $(document).ready(function () {
-        $("#file").on("change", handleFileSelect);
-        selDiv = $(".volume");
+        $("#input-file").on("change", handleFileSelect);
+        selDiv = $(".poster");
       });
 
       function handleFileSelect(e) {
@@ -174,3 +174,58 @@ class wall extends ReactCrop {
 }
 
 ReactDom.render(<wall />, document.getElementById('input-file'))*/
+
+let file_input = document.getElementById("file");
+  let image = document.getElementById("img");
+  let downloadButton = document.getElementById("download");
+  let aspectRatio = document.querySelectorAll(".aspect-ratio li");
+  let option = document.querySelector(".aspect-ratio");
+  const previewButton = document.getElementById("preview");
+  const previewImage = document.getElementById("preview-image");
+  let cropper = "";
+  let fileName = "";
+
+  file_input.onchange = () => {
+    previewImage.src = "";
+    downloadButton.classList.add("hide");
+
+    let reader = new FileReader();
+    reader.readAsDataURL(file_input.files[0]);
+    reader.onload = () => {
+      image.setAttribute("src", reader.result);
+      if (cropper) {
+        cropper.destroy();
+      }
+
+      cropper = new Cropper(image);
+      option.classList.remove("hide");
+      previewButton.classList.remove("hide");
+    };
+    fileName = file_input.files[0].name.split(".")[0];
+  };
+
+  aspectRatio.forEach((element) => {
+    element.addEventListener("click", () => {
+      if (element.innerText == "Custom") {
+        cropper.setAspectRatio(NaN);
+      } else {
+        cropper.setAspectRatio(eval(element.innerText.replace(":", "/")));
+      }
+    });
+  });
+
+  previewButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    downloadButton.classList.remove("hide");
+    let imgSrc = cropper.getCroppedCanvas({}).toDataURL();
+
+    previewImage.src = imgSrc;
+    downloadButton.download = `cropped_$(fileName).png`;
+    downloadButton.setAttribute("href", imgSrc);
+  });
+
+  window.onload = () => {
+    download.classList.add("hide");
+    option.classList.add("hide");
+    previewButton.classList.add("hide");
+  }
