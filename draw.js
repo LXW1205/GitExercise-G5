f34c93db657ec4752275b6febd31e5647b2661c8
Teng Ming Hein <tengminghein@gmail.com>
2024-05-14 00:50:26 +0800

    13-14/5 color selection

--- draw.js
+++ draw.js
@@ -1,7 +1,12 @@
-  const image_sel = document.getElementById('input-file');
-  const canvas = document.querySelector('#canvas');
-  const ctx = canvas.getContext('2d');    
+  const image_sel = document.getElementById("input-file");
+  const color_btn = document.querySelectorAll(".colors .option");
+  const color_picker = document.querySelector("#color-picker");
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
+  
+  //Global variables
   let img = null
+  let selected_color = "#000";
       
   image_sel.addEventListener('change', () => {
     const reader = new FileReader()
@@ -17,6 +22,22 @@
     reader.readAsDataURL(image_sel.files[0])
   })
 
+color_btn.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const select = document.querySelector(".colors .option .selected")
+      if (select){
+        select.classList.remove("selected");
+      }
+    btn.classList.add("selected");
+    selected_color = window.getComputedStyle(btn).getPropertyValue("background-color");
+  })
+})
+
+color_picker.addEventListener("change", () => {
+  color_picker.parentElement.style.background = color_picker.value;
+  color_picker.parentElement.click();
+})
+
   //Draw
   let drawing;
   canvas.onmousedown = (e) => {
@@ -24,11 +45,12 @@
     drawing = true;
     ctx.beginPath();
     ctx.lineWidth = 10;
-    ctx.strokeStyle = red;
+    ctx.strokeStyle = selected_color;
+    ctx.fillStyle = selected_color;
     ctx.lineJoin = "round";
     ctx.lineCap = "round";
     ctx.moveTo(e.offsetX, e.offsetY);
-  };
+  }
 
   canvas.onmousemove = (e) => {
     //console.log('mousemove', e.clientX, e.clientY);
@@ -36,10 +58,10 @@
       ctx.lineTo(e.offsetX, e.offsetY);
       ctx.stroke();
     }
-  };
+  }
 
   canvas.onmouseup = function () {
     //console.log('mousemove', e.clientX, e.clientY);
     drawing = false;
     ctx.closePath();
-  };  
+  }
