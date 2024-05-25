07c5cab90a8a85e74a1e0359418f687085346c98
Xiu Wei <xiuwei1205@gmail.com>
2024-05-25 23:15:39 +0800

    flattenicon

--- mergepdf.js
+++ mergepdf.js
@@ -5,8 +5,6 @@ async function timer() {
 async function removehide() {
   setTimeout(timer,1000);
 }
-    hide.classList.remove("hide");
-  }
 
 async function mergePDFs() {
     const fileInput1 = document.getElementById('file1');
