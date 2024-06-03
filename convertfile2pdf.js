744831d43a81c908c09a62ebffab41cd1fb82fb1
Jie Kit <jiekit16@gmail.com>
2024-06-03 21:26:12 +0800

    modify

--- convertfile2pdf.js
+++ convertfile2pdf.js
@@ -1,37 +1,12 @@
-const fileInput = document.getElementById('container');
-const selectedBanner = document.getElementById('output');
-
-fileInput.addEventListener('change', () => {
-  const files = fileInput.files;
-  const filesArr = Array.prototype.slice.call(files);
-  filesArr.forEach(function (f) {
-    if (!f.type.match("image.*")) {
-      return;
-    }
-    storedFiles.push(f);
-
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      var html =
-        '<img src="' +
-        e.target.result +
-        "\" data-file='" +
-        f.name +
-        "alt='Category Image' >";
-      selectedBanner.innerHTML = html;
-    };
-    reader.readAsDataURL(f);
-  });
-});
-
-
-function displayImage(event) {
+let uploadedFileName = '';
+  function displayImage(event) {
   const file = event.target.files[0];
   if (file) {
       const reader = new FileReader();
       reader.onload = function(e) {
           const output = document.getElementById('output');
           output.src = e.target.result;
+          uploadedFileName = file.name.split('.')[0];
       };
       reader.readAsDataURL(file);
   }
@@ -42,9 +17,10 @@ function convertToPDF() {
   const img = document.getElementById('output');
   const pdf = new jsPDF();
   if (img.src) {
-      pdf.addImage(img.src, 'JPEG', 15, 40, 180, 160);
-      pdf.save('download.pdf');
+      pdf.addImage(img.src, 'image/*', 15, 40, 180, 160);
+      const pdfFileName = uploadedFileName ? `${uploadedFileName}.pdf` : 'download.pdf';
+        pdf.save(pdfFileName);
   } else {
       alert("Please upload an image first.");
   }
-}
+}  
