dde3ec647aca3ed1a8726279860225f8afd96a57
Jie Kit <jiekit16@gmail.com>
2024-06-11 21:53:37 +0800

    last dayyyyy

--- convertfile2pdf.js
+++ convertfile2pdf.js
@@ -1,16 +1,36 @@
-let uploadedFileName = '';
-  function displayImage(event) {
-  const file = event.target.files[0];
-  if (file) {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-          const output = document.getElementById('output');
-          output.src = e.target.result;
-          uploadedFileName = file.name.split('.')[0];
+let uploadedFileName = "";
+let file = document.getElementById("input-file");
+
+file.addEventListener('change', displayImage);
+  function displayImage() {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const poster = document.querySelector('.poster img');
+      poster.src = e.target.result;
+      uploadedFileName = file.files[0].name.split('.')[0];
       };
-      reader.readAsDataURL(file);
+      reader.readAsDataURL(file.files[0]);
   }
-}
+
+//Drag and Drop to Import Image
+let drop_area = document.querySelector(".drop_area");
+drop_area.addEventListener("dragover", function(e){
+  e.preventDefault();
+  drop_area.innerText = "Release your image to upload";
+});
+
+drop_area.addEventListener("drop", function(e){
+  e.preventDefault();
+  file.files = e.dataTransfer.files;
+  uploadedFileName = file.files[0].name.split('.')[0];
+  drop_area.innerText = "Drag & Drop your image here";
+  displayImage();
+});
+
+drop_area.addEventListener("dragleave", function(e){
+  e.preventDefault();
+  drop_area.innerText = "Drag & Drop your image here";
+});
 
 function convertToPDF() {
   const { jsPDF } = window.jspdf;
